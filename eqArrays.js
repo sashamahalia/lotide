8ae1d03ce383493dd909bd0c5de9ae3f65cb12c7
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-03 01:45:30 +0000

    created function that compares if two arrays are equal

--- eqArrays.js
+++ eqArrays.js
@@ -5,3 +5,24 @@ const assertEqual = function(actual, expected) {
     console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
+
+const eqArrays = function(firstArray, secondArray) {
+  if (firstArray.length !== secondArray.length) {
+    return false
+  }
+
+  for (let i = 0; i < firstArray.length; i++) {
+    if (firstArray[i] !== secondArray[i]) {
+      return false;
+    }
+  }
+
+  return true;
+};
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) //=> should PASS
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) //=> should PASS
