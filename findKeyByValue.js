c4e851275222be6c3c3395a2ba80ab029a863433
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-03-04 02:55:02 +0000

    completed and linted findKeyByValue function

---
+++ findKeyByValue.js
@@ -0,0 +1,31 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`😇😇😇 Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`😭😭😭 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const findKeyByValue = function(object, value) {
+  let message = undefined;
+  for (const key in object) {
+    if (object[key] === value) {
+      message = key;
+      break;
+    }
+  }
+  return message;
+};
+
+// // test cases
+
+// const bestTVShowsByGenre = {
+//   sci_fi: "The Expanse",
+//   comedy: "Brooklyn Nine-Nine",
+//   drama:  "The Wire"
+// };
+
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'comedy');
+// assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'drama'); // should fail
