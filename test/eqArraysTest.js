ca49097b17debf5b7527ecd99e1ffa89fdfcbb55
Sasha Mahalia <sasha.camila.mahalia@gmail.com>
2021-05-26 22:37:19 +0000

    refactor as mocha test

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,21 +1,21 @@
-const assertEqual = require('../assertEqual');
 const eqArrays = require('../eqArrays');
+const assert = require('chai').assert;
 
-//test code
-const arr1 = [1, 2, 3, 4];
-const arr2 = [1, 2, 3, 4];
-assertEqual(eqArrays(arr1, arr2), true);
-
-console.log();
-
-arr2.push(5);
-assertEqual(eqArrays(arr1, arr2), false);
-
-console.log();
-
-const string1 = ['what', 'about', 'a', 'snack'];
-const a = 'a tasty';
-const string2 = ['what', 'about', a, 'snack'];
-assertEqual(eqArrays(string1, string2), false)
-
-
+describe('#eqArrays', () => {
+  it('should return true if both arrays are [1, 2, 3, 4]', () => {
+    const arr1 = [1, 2, 3, 4];
+    const arr2 = [1, 2, 3, 4];
+    assert.isTrue(eqArrays(arr1, arr2));
+  })
+  it('should return false if one array is [1, 2, 3, 4] and the other is [1, 2, 3, 4, 5]', () => {
+    const arr1 = [1, 2, 3, 4];
+    const arr2 = [1, 2, 3, 4, 5];
+    assert.isFalse(eqArrays(arr1, arr2));
+  })
+  it('should return false if two arrays are the same except for one containing a varible instead of a string', () => {
+    const string1 = ['what', 'about', 'a', 'snack'];
+    const a = 'a tasty';
+    const string2 = ['what', 'about', a, 'snack'];
+    assert.isFalse(eqArrays(string1, string2));
+  })
+});
